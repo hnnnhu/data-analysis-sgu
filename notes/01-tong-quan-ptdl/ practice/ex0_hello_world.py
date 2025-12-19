09d3610199eccf46349dc9032b1b02d9911712a5
Huỳnh Nguyễn Ngọc Như <165617497+hnnnhu@users.noreply.github.com>
2025-12-19 23:29:57 +0700

    Update ex0_hello_world.py

--- notes/01-tong-quan-ptdl/ practice/ex0_hello_world.py
+++ notes/01-tong-quan-ptdl/ practice/ex0_hello_world.py
@@ -1,1 +1,10 @@
+"""ex0_hello_world.py
+In ra lời chào đầu tiên để kiểm tra môi trường chạy Python.
+"""
+
+def main():
+    print("Hello, Python World!")
+
+if __name__ == "__main__":
+    main()
 
